a241bbbf652ce4fb89ef1e85427d73af80615f50
invrtd-h <hyegeun_song@gm.gist.ac.kr>
2023-03-02 17:13:51 +0900

    mo.h 수정

--- datastructures/mo.h
+++ datastructures/mo.h
@@ -4,13 +4,15 @@
 #include <bits/stdc++.h>
 
 template <typename T>
-struct Mospace {
+struct MoSpace {
     std::vector<T> data;
     int l{}, r{};
     
     virtual void insert(int val) = 0;
     virtual void erase(int val) = 0;
-    virtual T get_ans() = 0;
+    
+    [[nodiscard]]
+    virtual T get_ans() const = 0;
     
     void apply_query(int ql, int qr) {
         while (r < qr) insert(data[r++]);
