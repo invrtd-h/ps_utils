b313d06c953a183e42e166a526dd8d738d683a7f
invrtd-h <hyegeun_song@gm.gist.ac.kr>
2023-03-27 23:31:55 +0900

    230327

--- gens/maingen.py
+++ gens/maingen.py
@@ -1,19 +1,23 @@
 import sys
 import random as r
 from tcgen import random_fmt
-from treegen import tree_gen
+from treegen import tree_gen, path_gen, star_gen
 
 if __name__ == '__main__':
-    n = 10
+    n = 50000
+    t = 1
+
     f = open("../input.txt", "r")
     s = f.read()
     f.close()
 
-    sys.stdout = open("../input.txt", "w")
-    print(s)
+    s = s.split('#')
 
-    for i in range(100):
-        print('*')
+    sys.stdout = open("../input.txt", "w")
+    print(s[0], end='')
+
+    for i in range(t):
+        print('#')
         print(n)
         print(*(r.randint(1, n * 2) for _ in range(n)))
         tree_gen(n)
--- gens/treegen.py
+++ gens/treegen.py
@@ -6,5 +6,15 @@ def tree_gen(n):
         print(i + 2, lis[i] + 1)
 
 
+def path_gen(n):
+    for i in range(n - 1):
+        print(i + 1, i + 2)
+
+
+def star_gen(n):
+    for i in range(n - 1):
+        print(1, i + 2)
+
+
 if __name__ == '__main__':
     tree_gen(10)
